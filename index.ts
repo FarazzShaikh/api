8926a7b6ca713a198d2ef1c71056e2a6e3cae543
Faraz Shaikh <farazzshaikh@gmail.com>
2021-06-28 02:15:49 +0530

    Deploying to Heroku

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
 import { parse } from "https://deno.land/std/flags/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const { args } = Deno;
 const DEFAULT_PORT = 8000;
 const argPort = parse(args).port;
 
 const app = new Application();
+app.use(oakCors());
 
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
 console.log(port);
@@ -17,7 +19,6 @@ app
   .get("/github", async (r) => {
     const res = await (await fetch("https://github.com/FarazzShaikh")).text();
 
-    r.response.headers.append("Access-Control-Allow-Origin", "*");
     console.log(r.response);
     return res;
   })

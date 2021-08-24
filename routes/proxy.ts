dd8deff058535e89c78ead8d3d783e7a7809df72
Faraz Shaikh <farazzshaikh@gmail.com>
2021-08-25 01:16:10 +0400

    Updated GH Proxy url decode

--- routes/proxy.ts
+++ routes/proxy.ts
@@ -1,11 +1,20 @@
 import { Context } from "../Types.ts";
 import "https://deno.land/x/dotenv/load.ts";
 
+function urlDecode($s: string) {
+  return atob($s.replace("-", "+").replace("_", "/"));
+}
+
 export async function Proxy({ params, response }: Context) {
   let { route } = params;
+  console.log(params);
 
   if (route) {
-    route = atob(route);
+    if (route.includes("-") || route.includes("_")) {
+      route = urlDecode(route);
+    } else {
+      route = atob(route);
+    }
 
     const KEY = Deno.env.get("GITHUB_KEY");
     const url = "https://api.github.com/" + route;
